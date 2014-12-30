382197356b89289dadf5f20af6f2677e04d37dc6
Ingo Rößner <ingo.spicker@gmail.com>
2014-12-30 15:15:24 +0100

    seperate functions from List template

--- src/List.h
+++ src/List.h
@@ -10,7 +10,80 @@
 
 namespace LCollection{
 
+    template<typename Base>
+    class List;
+
     using std::to_string;
+
+    template<typename ListType, typename ElementType>
+    std::string ListToString(ListType* list , std::string delimiter = ", "){
+        std::string str = "";
+        bool first = true;
+        for(ElementType& t : *list){
+            if(!first){
+                str += delimiter;
+            }else{
+                first = false;
+            }
+            str += to_string(t);
+        }
+        return str;
+    }
+
+    template<typename ListType, typename ElementType>
+    Optional<ElementType> findFirstInList(ListType* list, std::function<bool(ElementType&)> f){
+        for(ElementType& t : *list){
+            if(f(t)){
+                return Optional<ElementType>::engage(t);
+            }
+        }
+        return Optional<ElementType>();
+    }
+
+    template<typename ListType, typename ElementType>
+    Optional<ElementType> findLastInList(ListType* list, std::function<bool(ElementType&)> f){
+        Optional<ElementType> optional;
+        for(ElementType& t : *list){
+            if(f(t)){
+                optional = Optional<ElementType>::engage(t);
+            }
+        }
+        return optional;
+    }
+
+    template<typename ListType, typename ElementType>
+    List<std::list<ElementType*>> findAllInList(ListType* list, std::function<bool(ElementType&)> f){
+        List<std::list<ElementType*>> result;
+        for(ElementType& t : *list){
+            if(f(t)){
+                result.push_back(&t);
+            }
+        }
+        return result;
+    }
+
+    template<typename ListType, typename ElementType>
+    List<std::list<ElementType>> findAllCopyInList(ListType* list, std::function<bool(ElementType&)> f){
+        List<std::list<ElementType>> result;
+        for(ElementType& t : *list){
+            if(f(t)){
+                result.push_back(t);
+            }
+        }
+        return result;
+    }
+
+    template<typename ListType, typename ElementType>
+    Optional<ElementType> getInList(ListType* list, unsigned int index){
+        unsigned int counter = 0;
+        for(ElementType& t : *list){
+            if(counter == index){
+                return Optional<ElementType>::engage(t);
+            }
+            counter++;
+        }
+        return Optional<ElementType>();
+    }
 
     template<typename Base>
     class List : public Base{
@@ -25,74 +98,33 @@ namespace LCollection{
         List(P&&... ps): Base({ps...}){};
 
         std::string toString(std::string delimiter = ", "){
-            std::string str = "";
-            bool first = true;
-            for(value_type& t : *this){
-                if(!first){
-                    str += delimiter;
-                }else{
-                    first = false;
-                }
-                str += to_string(t);
-            }
-            return str;
+            return ListToString<Base, value_type>(this, delimiter);
         }
 
         Optional<value_type> findFirst(std::function<bool(value_type&)> f){
-            for(value_type& t : *this){
-                if(f(t)){
-                    return Optional<value_type>::engage(t);
-                }
-            }
-            return Optional<value_type>();
+            return findFirstInList<Base, value_type>(this, f);
         }
 
         Optional<value_type> findLast(std::function<bool(value_type&)> f){
-            Optional<value_type> optional;
-            for(value_type& t : *this){
-                if(f(t)){
-                    optional = Optional<value_type>::engage(t);
-                }
-            }
-            return optional;
+            return findLastInList<Base, value_type>(this, f);
         }
 
         List<std::list<value_type*>> findAll(std::function<bool(value_type&)> f){
-            List<std::list<value_type*>> result;
-            for(value_type& t : *this){
-                if(f(t)){
-                    result.push_back(&t);
-                }
-            }
-            return result;
+            return findAllInList<Base, value_type>(this, f);
         }
 
         List<std::list<value_type>> findAllCopy(std::function<bool(value_type&)> f){
-            List<std::list<value_type>> result;
-            for(value_type& t : *this){
-                if(f(t)){
-                    result.push_back(t);
-                }
-            }
-            return result;
+            return findAllCopyInList<Base, value_type>(this, f);
         }
 
         /**
         * use only for ordered Base
         */
         Optional<value_type> get(unsigned int index){
-            unsigned int counter = 0;
-            for(value_type& t : *this){
-                if(counter == index){
-                    return Optional<value_type>::engage(t);
-                }
-                counter++;
-            }
-            return Optional<value_type>();
+            return getInList<Base, value_type>(this, index);
         }
 
-/*        LList<T> findAll(function<bool(T&)>);
-        LList<T*> findAll_ptr(function<bool(T&)>);
+/*        
         bool any(function<bool(T&)>);
         bool all(function<bool(T&)>);
         bool no(function<bool(T&)>);
